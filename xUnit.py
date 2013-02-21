90f604dea120edb841609c07f681316914f789b4
Roberto Pepato <rpepato@gmail.com>
2013-02-21 09:38:03 -0300

    Add minimal implementation of the xUnit framework in python (Book Exercises)

---
+++ xUnit.py
@@ -0,0 +1,109 @@
+class TestSuite:
+  def __init__(self):
+    self.tests= []
+
+  def add(self, test):
+    self.tests.append(test)
+
+  def run(self, result):
+    for test in self.tests:
+      test.run(result)
+
+class TestResult:
+  def __init__(self):
+    self.runCount= 0
+    self.errorCount= 0
+
+  def testStarted(self):
+    self.runCount= self.runCount + 1
+
+  def testFailed(self):
+    self.errorCount= self.errorCount + 1
+
+  def summary(self):
+    return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+class TestCase:
+  def __init__(self, name):
+    self.name= name
+
+  def run(self, result):
+    result.testStarted()
+    try:
+      self.setUp()
+      method = getattr(self, self.name)
+      method()    
+    except:
+      result.testFailed()
+    self.tearDown()
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+class WasRun(TestCase):
+  def __init__(self, name):
+    self.wasRun= None
+    TestCase.__init__(self, name)
+
+  def testMethod(self):
+    self.log= self.log + "testMethod "
+
+  def testBrokenMethod(self):
+    raise Exception
+
+  def setUp(self):
+    self.log= "setUp "
+
+  def tearDown(self):
+    self.log= self.log + "tearDown "
+
+class BrokenSetup(TestCase):
+  def __init__(self, name):
+    TestCase.__init__(self, name)
+
+  def setUp(self):
+    raise Exception
+
+class TestCaseTest(TestCase):
+  def setUp(self):
+    self.result= TestResult()
+  def testBrokenSetup(self):
+    test= BrokenSetup("setUp")
+    test.run(self.result)
+    assert("1 run, 1 failed" == self.result.summary())
+  def testTemplateMethod(self):
+    test= WasRun("testMethod")
+    test.run(self.result)
+    assert(test.log == "setUp testMethod tearDown ")
+  def testResult(self):
+    test = WasRun("testMethod")
+    result= test.run(self.result)
+    assert("1 run, 0 failed" == self.result.summary())
+  def testFailedResult(self):
+    test= WasRun("testBrokenMethod")
+    test.run(self.result)
+    assert("1 run, 1 failed" == self.result.summary())
+  def testFailedResultFormatting(self):
+    self.result.testStarted()
+    self.result.testFailed()
+    assert("1 run, 1 failed" == self.result.summary())
+  def testSuite(self):
+    suite= TestSuite()
+    suite.add(WasRun("testMethod"))
+    suite.add(WasRun("testBrokenMethod"))
+    suite.run(self.result)
+    assert("2 run, 1 failed" == self.result.summary())
+
+suite= TestSuite()
+suite.add(TestCaseTest("testBrokenSetup"))
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result= TestResult()
+suite.run(result)
+print result.summary()
